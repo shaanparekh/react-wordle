80f4f95e2bcb41b5b3822c203873b39f4f61d0dc
shaanparekh <shaanparekh@utexas.edu>
2022-05-25 15:02:29 -0500

    info change

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -19,43 +19,42 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="H"
           status="correct"
         />
-        <Cell value="E" />
         <Cell value="A" />
-        <Cell value="R" />
+        <Cell value="P" />
+        <Cell value="P" />
         <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter H is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="A" />
+        <Cell value="N" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="N"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="I" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter N is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="N" />
+        <Cell isRevealing={true} isCompleted={true} value="S" status="absent" />
+        <Cell value="H" />
+        <Cell value = "U" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter S is not in the word in any spot.
       </p>
     </BaseModal>
   )
